c44c92912e214b2f44ccdd260051d8fd5b75f0bf
lamm1685 <lamm1685@SSCB03-45599>
2019-09-26 19:58:48 -0500

    participation 4

---
+++ Participation4/A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A");
+            Console.Write("Enter a number: ");
+            int n=int.Parse(Console.ReadLine());
+            int digit = n%10;
+            int stop = n / 10;
+            int sum=0;
+            sum += digit;
+            bool check = true;
+            while (stop > 0)
+            {
+                digit = stop % 10;
+                if (digit < 1) sum += stop;
+                else sum += digit;
+                stop /= 10;
+            }
+            Console.WriteLine($"Sum of all digits is {sum}");
+            if (sum % 3 != 0) check = false;
+            Console.WriteLine($"Sum%3? {check}");
+        }
+    }
+}
---
+++ Participation4/B/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B");
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            int digit = n % 10;
+            int stop = n / 10;
+            int sum = 0;
+            int sum2 = 0;
+            int count = 0;
+            if (count%2==0) sum += digit;
+            bool check = true;
+            
+            while (stop > 0)
+            {
+                count++;
+                digit = stop % 10;
+                if (count % 2 == 0) sum += digit;
+                else sum2 += digit;
+                stop /= 10;
+               
+            }
+            Console.WriteLine($"Sum of all odd digits is {sum2}");
+            Console.WriteLine($"Sum of all even digits is {sum}");
+            int dif = sum-sum2;
+            Console.WriteLine($"Dif of all digits is {dif}");
+            if (dif % 11 != 0) check = false;
+            Console.WriteLine($"Dif%11? {check}");
+        }
+    }
+}
