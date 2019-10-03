daffca25d662824bef09a68175437b0037e808f1
khietlam1 <khietsune@gmail.com>
2019-10-03 12:05:42 -0500

    HW04 final submit

--- HW04/HW04/Program.cs
+++ HW04/HW04/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
-
-namespace HW04
+using static System.Console;
+namespace B._2._3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("B.2.3.a");
+
+            char ch, ch2 = 'A';
+            bool vowel = false;
+            bool vowel2 = false;
+            WriteLine("at least 1 vowel");
+            for (ch = 'A'; ch <= 'H'; ch++)
+            {
+                if (ch == 'A' || ch == 'E') vowel = true;
+
+                for (ch2 = 'A'; ch2 <= 'H'; ch2++)
+                {
+
+                    if (ch == 'A' || ch == 'E' || ch2 == 'A' || ch2 == 'E')
+                    {
+                        Write($"{ch}{ch2} ");
+                    }
+
+
+                }
+            }
+            WriteLine();
+            WriteLine("1 consonant, 1 vowel");
+            for (ch = 'A'; ch <= 'H'; ch++)
+            {
+                if (ch == 'A' || ch == 'E') vowel = true;
+
+                for (ch2 = 'A'; ch2 <= 'H'; ch2++)
+                {
+                    if (ch2 == 'A' || ch2 == 'E') vowel2 = true;
+                    if ((vowel == false && vowel2 == true) || (vowel == true && vowel2 == false))
+                    {
+                        Write($"{ch}{ch2} ");
+
+                    }
+                    vowel2 = false;
+                }
+                vowel = false;
+            }
+            WriteLine();
+            WriteLine("Begin Consonant, End Vowel");
+            for (ch = 'A'; ch <= 'H'; ch++)
+            {
+                if (ch != 'A' && ch != 'E')
+                {
+
+                    for (ch2 = 'A'; ch2 <= 'H'; ch2++)
+                    {
+                        if (ch2 == 'A' || ch2 == 'E')
+
+                        {
+                            Write($"{ch}{ch2} ");
+
+                        }
+
+                    }
+
+                }
+            }
+
+
+
+
         }
     }
 }
